a6e0e390af825507e1b9b08622e8d3a2799bf779
Martin <martinius96@gmail.com>
2020-03-14 16:40:15 +0100

    Update Arduino_Ethernet_W5100.ino

--- Arduino_Ethernet_W5100/Arduino_Ethernet_W5100.ino
+++ Arduino_Ethernet_W5100/Arduino_Ethernet_W5100.ino
@@ -10,58 +10,43 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include "HX711.h"
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char server[] = "www.arduino.php5.sk";
 IPAddress ip(192, 168, 1, 101);
 EthernetClient client;
+#define DOUT  3
+#define CLK  2
 HX711 scale;
+float calibration_factor = -50.10; //KALIBRACNY FAKTOR
+
 void setup() {
   Serial.begin(115200);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());      //raw from ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));   //average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));   //average of 5 readings from the ADC
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);  //average of 5 readings from the ADC
-  // by the SCALE parameter (not set yet)
-
-  scale.set_scale(2280.f);  // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();  // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 //raw reading from ADC
-
+  scale.begin(DOUT, CLK);
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
   }
   Serial.print("HX711 ready");
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
   wdt_enable(WDTO_8S);
 }
 
 void loop() {
-   if (Ethernet.begin(mac) == 0) {
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+  if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
   }
   wdt_reset();
-  float hodnota = scale.get_units(10);
-  String hodnota_odosielanie = String(hodnota));
+  float hodnota = (scale.get_units(10), 2);
+  String hodnota_odosielanie = String(hodnota);
   delay(50);
   if (client.connect(server, 80)) {
-    String hodnota = String(vzdialenost);
+    wdt_reset();
     Serial.println("Pripojenie uspesne na webserver, vykonavam request... ");
     client.print("GET /vaha/data.php?hodnota=");
     client.print(hodnota_odosielanie);
@@ -74,11 +59,10 @@ void loop() {
   } else {
     Serial.println("Pripojenie zlyhalo...");
   }
-}
-scale.power_down();
-for (int i = 0; i <= 300; i++) {
-  delay(1000);
-  wdt_reset();
-}
-scale.power_up();
+  scale.power_down();
+  for (int i = 0; i <= 300; i++) {
+    delay(1000);
+    wdt_reset();
+  }
+  scale.power_up();
 }
