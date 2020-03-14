0b7fdbaa367f28194a5142691d742cf2f63de612
Martin <martinius96@gmail.com>
2020-03-14 17:06:03 +0100

    Update Arduino_Ethernet_W5100.ino

--- Arduino_Ethernet_W5100/Arduino_Ethernet_W5100.ino
+++ Arduino_Ethernet_W5100/Arduino_Ethernet_W5100.ino
@@ -1,15 +1,20 @@
-/*|---------------------------------|*/
-/*|Projekt: Včelárska váha          |*/
-/*|Autor: Martin Chlebovec          |*/
-/*|E-mail: martinius96@gmail.com    |*/
-/*|Web: https://arduino.php5.sk     |*/
-/*|Licencia pouzitia: MIT           |*/
-/*|---------------------------------|*/
+/*|----------------------------------|*/
+/*|Projekt: Včelárska váha           |*/
+/*|Hardvér: Arduino + Ethernet W5100 |*/
+/*|Autor: Martin Chlebovec           |*/
+/*|E-mail: martinius96@gmail.com     |*/
+/*|Web: https://arduino.php5.sk      |*/
+/*|Licencia pouzitia: MIT            |*/
+/*|Revízia: 14. Marec 2020           |*/
+/*|----------------------------------|*/
 
 #include <avr\wdt.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include "HX711.h"
+
+float calibration_factor = -50.10;
+unsigned long zero_factor =  -90709;
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 char server[] = "www.arduino.php5.sk";
 IPAddress ip(192, 168, 1, 101);
@@ -17,26 +22,20 @@ EthernetClient client;
 #define DOUT  3
 #define CLK  2
 HX711 scale;
-float calibration_factor = -50.10; //KALIBRACNY FAKTOR
-
 void setup() {
   Serial.begin(115200);
   scale.begin(DOUT, CLK);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.set_offset(zero_factor); //Zero out the scale using a previously known zero_factor
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
   }
   Serial.print("HX711 ready");
-  scale.set_scale();
-  scale.tare(); //Reset the scale to 0
-  long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
   wdt_enable(WDTO_8S);
 }
 
 void loop() {
-  scale.set_scale(calibration_factor); //Adjust to this calibration factor
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     Ethernet.begin(mac, ip);
