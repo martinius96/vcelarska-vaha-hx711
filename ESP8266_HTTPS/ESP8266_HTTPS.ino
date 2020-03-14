9e8489389bb83205a4f6736c5f242e3798ed32a9
Martin <martinius96@gmail.com>
2020-03-14 23:22:50 +0100

    Update ESP8266_HTTPS.ino

--- ESP8266_HTTPS/ESP8266_HTTPS.ino
+++ ESP8266_HTTPS/ESP8266_HTTPS.ino
@@ -14,8 +14,8 @@
 
 float calibration_factor = -50.10;
 unsigned long zero_factor =  -90709;
-const char * ssid = "MenoWifiSiete";
-const char * password = "HesloWifiSiete";
+const char * ssid = "huawei-2929";
+const char * password = "chefrolet";
 const char * host = "arduino.php5.sk"; //bez https a www
 const int httpsPort = 443; //https port
 const char fingerprint[] PROGMEM = "b0 6d 7f 8c 98 78 8e 6e 0a 57 a8 2f 7e d1 40 2a 1e 3f 48 f7";
@@ -43,9 +43,13 @@ void loop() {
   float hodnota = (scale.get_units(10), 2);
   String hodnota_odosielanie = String(hodnota);
   delay(50);
+  WiFiClientSecure client;
+  Serial.printf("Using fingerprint '%s'\n", fingerprint);
+  client.setFingerprint(fingerprint);
   if (client.connect(host, httpsPort)) {
     String url = "/vaha/data.php?hodnota=" + hodnota_odosielanie;
     client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: NodeMCU\r\n" + "Connection: close\r\n\r\n");
+    Serial.println("Data uspesne odoslane na webserver");
     while (client.connected()) {
       String line = client.readStringUntil('\n');
       if (line == "\r") {
@@ -57,10 +61,9 @@ void loop() {
     Serial.println("Problem s pripojenim na webserver");
   }
   client.stop();
-}
-scale.power_down();
-for (int i = 0; i <= 300; i++) {
-  delay(1000);
-}
-scale.power_up();
+  scale.power_down();
+  for (int i = 0; i <= 300; i++) {
+    delay(1000);
+  }
+  scale.power_up();
 }
